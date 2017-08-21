71c485789421e9428d773ef3970a26426fa49386
Chris Handy <maximinus@gmail.com>
2017-08-21 19:04:05 +0100

    Made Window example work

--- wargame-examples/07_window/window.py
+++ wargame-examples/07_window/window.py
@@ -4,18 +4,21 @@ import os
 
 import wargame.engine
 from wargame.scene import Scene
-from wargame.gui.window import Window
 from wargame.nodes import ImageNode
+from wargame.gui.containers import Window
+from wargame.gui.nodes import GuiImage
 
 
 def game():
-    controller = wargame.engine.init(os.getcwd())
+    resources = os.path.join(os.getcwd(), '../')
+    controller = wargame.engine.init(resources)
+
     # let's have a background for a change.
     # a very simple ImageNode will do
     background = ImageNode.from_image(0, 0, 'sprites.wallpaper')
     # this time, we want a window to display
     # but a window must display 'something', so we'll use an ImageNode
-    window_contents = ImageNode.from_image(0, 0, 'sprites.dog')
+    window_contents = GuiImage.from_image('sprites.dog')
     window = Window(window_contents)
     # add the window to a scene
     scene = Scene([background, window])
