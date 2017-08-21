e3b01d8dc28a8f68e64667aeeb4b7d27cb3ee462
Chris Handy <maximinus@gmail.com>
2017-08-21 19:22:40 +0100

    Made nested containers example work

--- wargame-examples/10_nested_containers/nested.py
+++ wargame-examples/10_nested_containers/nested.py
@@ -5,30 +5,32 @@ import os
 import wargame.engine
 from wargame.scene import Scene
 from wargame.nodes import ImageNode
-from wargame.gui.gui_nodes import GuiNode, HorizontalContainer, VerticalContainer
+from wargame.gui.nodes import GuiImage, HorizontalContainer, VerticalContainer
 from wargame.gui.layout import Align
-from wargame.gui.window import Window
+from wargame.gui.containers import Window
 from wargame.loader import Resources
 
 
 def game():
-    controller = wargame.engine.init(os.getcwd())
+    resources = os.path.join(os.getcwd(), '../')
+    controller = wargame.engine.init(resources)
+
     # let's have a background
     background = ImageNode.from_image(0, 0, 'sprites.wallpaper')
     # we want a window to display with a HorizontalContainer
-    node1 = GuiNode.from_image(Resources.colour_surface(100, 30, (200, 32, 32)))
-    node2 = GuiNode.from_image(Resources.colour_surface(100, 30, (32, 200, 32)))
-    node3 = GuiNode.from_image(Resources.colour_surface(100, 30, (32, 32, 200)))
+    node1 = GuiImage(Resources.colour_surface(100, 30, (200, 32, 32)))
+    node2 = GuiImage(Resources.colour_surface(100, 30, (32, 200, 32)))
+    node3 = GuiImage(Resources.colour_surface(100, 30, (32, 32, 200)))
     c1 = VerticalContainer([node1, node2, node3], (214, 214, 214), align_children=Align.TOP)
 
-    node6 = GuiNode.from_image(Resources.colour_surface(100, 50, (32, 32, 200)))
-    node5 = GuiNode.from_image(Resources.colour_surface(100, 50, (32, 200, 32)))
-    node4 = GuiNode.from_image(Resources.colour_surface(100, 100, (200, 32, 32)))
+    node6 = GuiImage(Resources.colour_surface(100, 50, (32, 32, 200)))
+    node5 = GuiImage(Resources.colour_surface(100, 50, (32, 200, 32)))
+    node4 = GuiImage(Resources.colour_surface(100, 100, (200, 32, 32)))
     c2 = VerticalContainer([node6, node5, node4], (214, 214, 214), align_children=Align.TOP)
 
-    node7 = GuiNode.from_image(Resources.colour_surface(100, 30, (200, 32, 32)))
-    node8 = GuiNode.from_image(Resources.colour_surface(100, 30, (32, 200, 32)))
-    node9 = GuiNode.from_image(Resources.colour_surface(100, 30, (32, 32, 200)))
+    node7 = GuiImage(Resources.colour_surface(100, 30, (200, 32, 32)))
+    node8 = GuiImage(Resources.colour_surface(100, 30, (32, 200, 32)))
+    node9 = GuiImage(Resources.colour_surface(100, 30, (32, 32, 200)))
     c3 = VerticalContainer([node7, node8, node9], (214, 214, 214), align_children=Align.TOP)
 
     hcontainer = HorizontalContainer([c1, c2, c3], (214, 214, 214), align_children=Align.TOP)
